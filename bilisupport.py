dce3bbe1a678dbcdee3917c7570cc40e3ee1b40a
The-Earth <tigerzeng97@gmail.com>
2018-11-18 18:14:00 +0800

    bilibili data fetch initial commit
    reference from https://github.com/TimeCompass/bilibili-data

---
+++ bilisupport.py
@@ -0,0 +1,50 @@
+'''Bilibili API列表'''
+
+# APPKEY = '12737ff7776f1ade'
+APPKEY = '8e9fc618fbd41e28'
+
+# CID_DANMAKU.format(cid)
+CID_DANMAKU = 'http://comment.bilibili.com/{0}.xml'
+# CID_DANMAKU_HIS.format(cid)
+CID_DANMAKU_HIS = 'http://comment.bilibili.com/rolldate,{0}'
+# CID_HISDANMAKU.format(timestamp, cid)
+CID_HISDANMAKU = 'http://comment.bilibili.com/dmroll,{0},{1}'
+
+# {'aid': aid}
+API_PAGELIST = 'http://www.bilibili.com/widget/getPageList?'
+
+# {'mid': mid, 'pagesize': pagesize, 'tid': tid, 'page': page}
+API_SUBMITVIDEOS = 'http://space.bilibili.com/ajax/member/getSubmitVideos?'
+
+# {'type': 'json', 'appkey': APPKEY, 'id': aid} & cookies needed
+API_VIDEOSTATUS = 'http://api.bilibili.com/view?'
+
+# {'mid': mid} {'Referer': 'http://space.bilibili.com/{mid}/'} POST method
+API_SPACE = 'http://space.bilibili.com/ajax/member/GetInfo'
+
+# {'jsonp': jsonp, 'type': 1, 'sort': 0, 'oid': oid, 'pn': page, 'nohot': 1}
+API_REPLY = 'http://api.bilibili.com/x/v2/reply?'
+
+# {'aid': aid, 'jsonp': 'jsonp'}
+API_TAG = 'http://api.bilibili.com/x/tag/archive/tags?'
+# {'id': tid, 'jsonp': 'jsonp'}
+API_TAGINFO = 'http://api.bilibili.com/tags/info_description?'
+# {'aid': aid, 'tag_id': tagid, 'jsonp': 'jsonp'} POST method & cookies needed
+API_TAGDEL = 'http://api.bilibili.com/x/tag/archive/del'
+# {'aid': aid, 'tag_name': tagname, 'jsonp': 'jsonp'} POST method & cookies needed
+API_TAGADD = 'http://api.bilibili.com/x/tag/archive/add'
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+     (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+    'Cookie': 'SESSDATA=; \
+    LIVE_BUVID=; \
+    LIVE_BUVID__ckMd5=; \
+    DedeUserID=; \
+    DedeUserID__ckMd5='
+    }
+
+PROXIES = {
+    'http': 'socks5://127.0.0.1:1080',
+    'https': 'socks5://127.0.0.1:1080'
+}
---
+++ vid.py
@@ -0,0 +1,34 @@
+'''遍历全站AID'''
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from bilisupport import API_VIDEOSTATUS, HEADERS, APPKEY
+import requests
+
+
+def getinfo(aid):
+    '''获取aid信息'''
+    if not aid:
+        return 404
+    else:
+        aid = int(aid)
+    paramsinfo = {'type': 'json', 'appkey': APPKEY, 'id': aid}
+    gsvres = requests.get(url=API_VIDEOSTATUS, params=paramsinfo, headers=HEADERS).json()
+    if gsvres.get('code') is None:
+        postdata = {
+            'comment': int(gsvres.get('video_review')),
+            'video_review': int(gsvres.get('video_review')),
+            'coins': int(gsvres.get('coins')),
+            'favorites': int(gsvres.get('favorites')),
+            'tid': int(gsvres.get('tid')),
+            'typename': ''
+        }
+        if gsvres.get('play') != "--":
+            postdata['play'] = gsvres.get('play')
+    else:
+        print(aid, gsvres.get('code'), gsvres.get('error'))
+        return 404
+
+
+if __name__ == '__main__':
+    pass
