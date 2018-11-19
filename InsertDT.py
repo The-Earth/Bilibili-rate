87ce569c27395c8ed6a36ebd009a6da97c662bab
The-Earth <tigerzeng97@gmail.com>
2018-11-19 09:36:13 +0800

    fix gitignore & use import getinfo(aid)

--- InsertDT.py
+++ InsertDT.py
@@ -1,6 +1,5 @@
 import sqlite3
-from bilisupport import API_VIDEOSTATUS, HEADERS, APPKEY
-import requests
+from vid import getinfo
 
 def InsertData(aid):
     conn = sqlite3.connect('Data.db')
@@ -24,39 +23,6 @@ def InsertData(aid):
         conn.commit()
         conn.close()
 
-def getinfo(aid):
-    '''
-    {comment:'Here is comment',
-    {coins: 123,
-    ...
-    }
-    '''
-    if not aid:
-        return 404
-    else:
-        aid = int(aid)
-    paramsinfo = {'type': 'json', 'appkey': APPKEY, 'id': aid}
-    gsvres = requests.get(url=API_VIDEOSTATUS, params=paramsinfo, headers=HEADERS).json()
-    if gsvres.get('code') is None:
-        postdata = {
-            'comment': int(gsvres.get('video_review')),
-            'video_review': int(gsvres.get('video_review')),
-            'coins': int(gsvres.get('coins')),
-            'favorites': int(gsvres.get('favorites')),
-            'tid': int(gsvres.get('tid')),
-            'typename': str(gsvres.get('typname')),
-            'arctype': str(gsvres.get('arctype')),
-            'review': int(gsvres.get('review')),
-            'title': str(gsvres.get('title')),
-            'description': str(gsvres.get('description')),
-            'mid': int(gsvres.get('mid')),
-        }
-        if gsvres.get('play') != "--":
-            postdata['play'] = int(gsvres.get('play'))
-    else:
-        print(aid, gsvres.get('code'), gsvres.get('error'))
-        return 404
-    return postdata
 
 f=open('AlreadyIn.txt','a')
 print("################数据库扩容操作################")
